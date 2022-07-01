11c5284ed6125d8f8b8d8799bdb957b55cdd552c
Stéphane <stephane.sokeo@gmail.com>
2022-07-01 16:20:28 +0200

    add header component

--- snapcloud/src/app/app.module.ts
+++ snapcloud/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { CloudSnapComponent } from './cloud-snap/cloud-snap.component';
 import { registerLocaleData } from '@angular/common';
 import * as fr from '@angular/common/locales/fr';
 import { CloudSnapListComponent } from './cloud-snap-list/cloud-snap-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, CloudSnapComponent, CloudSnapListComponent],
+  declarations: [AppComponent, CloudSnapComponent, CloudSnapListComponent, HeaderComponent],
   imports: [BrowserModule],
   providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }],
   bootstrap: [AppComponent],
