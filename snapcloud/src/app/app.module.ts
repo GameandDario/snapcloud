30fff74f05534447ae7b7c86ef110c86ce1878ec
Stéphane <stephane.sokeo@gmail.com>
2022-07-01 08:10:05 +0200

    add pipe to format date and uppercase

--- snapcloud/src/app/app.module.ts
+++ snapcloud/src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CloudSnapComponent } from './cloud-snap/cloud-snap.component';
+import { registerLocaleData } from '@angular/common';
+import * as fr from '@angular/common/locales/fr';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CloudSnapComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, CloudSnapComponent],
+  imports: [BrowserModule],
+  providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    registerLocaleData(fr.default);
+  }
+}
