05689055db4c19fed9b9fb0dc7175f71dd59b6ac
Stéphane <stephane.sokeo@gmail.com>
2022-07-14 15:23:06 +0200

    add tap obervable for sideEffects

--- snapcloud/src/app/app.component.ts
+++ snapcloud/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { interval, Observable } from 'rxjs';
-import { map, filter } from 'rxjs/operators';
+import { map, filter, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -15,12 +15,17 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.interval$ = interval(1000).pipe(
       //ordre des opérateur est important : c'est dans cet ordre que les opérations seront effectuées
+      //src Observavbles : https://www.learnrxjs.io
       filter((value) => value % 3 === 0),
       map((value) =>
         value % 2 === 0
           ? `Je suis ${value} et je suis pair`
           : `Je suis ${value} et je suis impair`
-      )
+      ),
+      tap((text) => this.logger(text))
     );
+  }
+  logger(text: string): void {
+    console.log(`Log : ${text}`);
   }
 }
