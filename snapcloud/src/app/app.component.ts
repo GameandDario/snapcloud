c1e76db8e1615aec652e469fe20a7c8715738698
Stéphane <stephane.sokeo@gmail.com>
2022-07-14 14:49:59 +0200

    add observable w/ async pipe

--- snapcloud/src/app/app.component.ts
+++ snapcloud/src/app/app.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  //Déclaration de propriété
+
+  interval$!: Observable<number>;
+
+  ngOnInit(): void {
+    this.interval$ = interval(1000);
+  }
+}
