48cf4c3fef2b00c338ff1cf34cacfb624a014a52
Stéphane <stephane.sokeo@gmail.com>
2022-07-14 15:05:09 +0200

    show every observable value w/ map operator

--- snapcloud/src/app/app.component.ts
+++ snapcloud/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { interval, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,15 @@ import { interval, Observable } from 'rxjs';
 export class AppComponent implements OnInit {
   //Déclaration de propriété
 
-  interval$!: Observable<number>;
+  interval$!: Observable<string>;
 
   ngOnInit(): void {
-    this.interval$ = interval(1000);
+    this.interval$ = interval(1000).pipe(
+      map((value) =>
+        value % 2 === 0
+          ? `Je suis ${value} et je suis pair`
+          : `Je suis ${value} et je suis impair`
+      )
+    );
   }
 }
