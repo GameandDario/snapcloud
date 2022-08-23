b87ced9cd0856a0876bab744d171ccbac72e4295
Stéphane <stephane.sokeo@gmail.com>
2022-08-23 17:49:25 +0200

    add routing for new cloudSnap

--- snapcloud/src/app/app-routing.module.ts
+++ snapcloud/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CloudSnapListComponent } from './cloud-snap-list/cloud-snap-list.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NewCloudSnapComponent } from './new-cloud-snap/new-cloud-snap.component';
 import { SingleCloudSnapComponent } from './single-cloud-snap/single-cloud-snap.component';
 
 const routes: Routes = [
   { path: 'cloudsnaps', component: CloudSnapListComponent },
   { path: '', component: LandingPageComponent },
   { path: 'cloudsnaps/:id', component: SingleCloudSnapComponent },
+  { path: 'create', component: NewCloudSnapComponent },
 ];
 
 @NgModule({
